56aa010c65e8be070872f809edda4178b4fd1d8e
Mfahmi150703 <151372759+Mfahmi150703@users.noreply.github.com>
2023-11-20 21:17:25 +0700

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+$DBHOST = 'localhost';
+$DBUSER = 'root';
+$DBPASSWORD = '';
+$DBNAME = 'tugas_db';
+
+// $db_connect = mysqli_connect($DBHOST,$DBUSER,$DBPASSWORD,$DBNAME);
+$db_connect = mysqli_connect($DBHOST,$DBUSER,$DBPASSWORD,$DBNAME);
+
+if(mysqli_connect_errno()){
+    echo 'Gagal Login / Register'.mysqli_connect_error();
+}
+?>
